8f54f2e10a4e5d4a4f770b6a4113e58efb1b84f6
motazabusaleh <motaz989898@gmail.com>
2020-12-17 13:45:59 +0200

    mysite

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
 alert("hello there")
-prompt("welcome "+ prompt("what is your name?")) 
+var username = prompt("what is your name ")
+alert(username)
 
+var age =(prompt("how old are you?"))
+if(age<20){
+    alert("you cant enter my site")
+}
+else{
+    alert("welcome to my site")
+}
+
+var favfood =prompt("what is your fav. food?")
 
 
 
