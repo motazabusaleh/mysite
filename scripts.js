d72f80a8483673a3e8e001254e8e6a00be707536
motazabusaleh <motaz989898@gmail.com>
2020-12-17 14:04:09 +0200

    mysite

--- scripts.js
+++ scripts.js
@@ -2,17 +2,25 @@ alert("hello there")
 var username = prompt("what is your name ")
 alert(username)
 
-var age =(prompt("how old are you?"))
-if(age<20){
+var age = (prompt("how old are you?"))
+if (age < 20) {
     alert("you cant enter my site")
 }
-else{
+else {
     alert("welcome to my site")
 }
 
-var favfood =prompt("what is your fav. food?")
+var favfood = prompt("what is your fav. food?")
 alert("favfood")
 
+function orderfood() {
 
+    var userorder = prompt("enter your order=mansaf, burger")
 
- 
+    while (userorder != "mansaf" && userorder != "burger") {
+        userorder = prompt("enter your order=mansaf, burger")
+    }
+
+}
+orderfood();
+
